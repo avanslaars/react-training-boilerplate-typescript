66e5f51ad2db004d60395a33f3bcd8925725ad94
Andrew Van Slaars <avanslaars@gmail.com>
2017-11-06 22:17:40 -0500

    Added json-server and css support

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,9 @@ module.exports = {
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
-      // { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ }
     ]
   },
   // Enable sourcemaps for debugging webpack's output.
